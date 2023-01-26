4bc2683e7328d33ff4ffa31995a95e3870cfe247
Sterling Newsom <sterlingnewsom@Sterlings-MacBook-Pro.local>
2023-01-26 01:36:44 -0800

    merged vs code changes

---
+++ src/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+return (
+    <header>
+    <div className="container">
+        <nav>
+            <h1 className = "logo">KHANBAN</h1>
+            <button className = "btn">Add new task</button>
+        </nav>
+        </div>
+    </header>
+)
+}
+
+export default Header
