b43193232030810562af08dbc50e75fed040190b
Sterling Newsom <sterlingnewsom@Sterlings-MacBook-Pro.local>
2023-01-26 08:46:51 -0800

    added new key

---
+++ Task.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Task = () => {
+return (
+    <div className = "container">
+        <div className = "cards">
+        <Todo />
+
+        </div>
+    </div>
+)
+}
+
+
+const Todo = () =>{
+    <h1>To-do</h1>
+    <div className='card'
+
+}
+
+
+export default Task
--- src/Header.jsx
+++ src/Header.jsx
@@ -5,8 +5,8 @@ return (
     <header>
     <div className="container">
         <nav>
-            <h1 className = "logo">KHANBAN</h1>
-            <button className = "btn">Add new task</button>
+            <h1 className = "logo"><i class="fa-solid fa-spa"></i> KHANBAN</h1>
+            <button className = "btn"><i class="fa-solid fa-plus"></i> Add new task</button>
         </nav>
         </div>
     </header>
